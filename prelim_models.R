44dfaf8793c43ca1957c77f4cfd6adb67ab8e3c1
M-Krogman <krogmanm@carleton.edu>
2025-08-18 11:22:50 -0500

    Wrote glm stuff, pre_running glm_fit_workflows 8/18

--- prelim_models.R
+++ prelim_models.R
@@ -71,6 +71,10 @@ summary(harmful_hospit_model)
 # note at end of work session: ihospit_model is our best/most intriguing so far,
 # but should also look more into that mobility_index variable
 
+
+
+
+
 # I will actually do some ML here
 # A model to predict the number of hospital visits, regardless of emergency or no
 # would be helpful for determining staff/other resource needs
@@ -163,3 +167,88 @@ results <- bind_cols(airq_hospit, predictions)
 # so now .pred gives us an approximation of how many people will be in hospitals throughout the entire day
 # so, they'll know how to allocate their resources appropriately.
 # the only thing that could make this more specific would be to have hour-by-hour data
+
+
+
+
+
+# Going to make another machine learning model, for predicting lockdowns
+
+airq_lockdown <- airq %>% 
+  mutate(lockdown_status = as.factor(lockdown_status)) %>% 
+  select(-c(year, day))   # removing these because I fail to see how they would be any more than incidental
+                          # same reason as in the last model too
+
+index2 <- initial_split(airq_lockdown, prop = .8)
+train2 <- training(index2)
+test2 <- testing(index2)
+cv_folds2 <- vfold_cv(data = train2, v = 10)
+
+glm_rec1 <- recipe(lockdown_status ~ ., data = train2) %>% 
+  step_dummy(region, month)
+
+glm_rec2 <- glm_rec1 %>% 
+  step_normalize(all_numeric_predictors()) %>% 
+  step_nzv(all_numeric_predictors())
+
+glm_rf_spec <- rand_forest(
+  mtry = tune(),
+  min_n = tune(),
+  mode = 'classification',
+  engine = 'ranger'
+)
+
+glm_log_spec <- logistic_reg(
+  mode = 'classification',
+  engine = 'glm'
+)
+
+glm_log_spec2 <- logistic_reg(
+  mode = 'classification',
+  engine = 'glmnet'
+)
+
+glm_knn_spec <- nearest_neighbor(
+  mode = 'classification',
+  engine = 'kknn',
+  neighbors = tune()
+)
+
+glm_wf_set <- workflow_set(
+  preproc = list(glm_rec1, glm_rec2),
+  models = list(glm_rf_spec, glm_log_spec, glm_log_spec2, glm_knn_spec),
+  cross = TRUE
+)
+
+glm_fit_workflows <- glm_wf_set %>% 
+  workflow_map(
+    fn = 'tune_grid',
+    grid = 10,
+    resamples = cv_folds2,
+    verbose = TRUE
+  )
+
+glm_best_wf_id <- rank_results(fit_workflows, rank_metric = 'rmse',
+                           select_best = TRUE) %>% 
+  slice_head(n = 1) %>% 
+  pull(wflow_id)
+
+glm_wf_best <- extract_workflow(fit_workflows,   # Gotta pull out the best workflow
+                            id = best_wf_id)
+
+glm_wf_best_tuned <- fit_workflows[glm_fit_workflows$wflow_id == glm_best_wf_id,
+                               'result'][[1]][[1]]
+
+collect_metrics(glm_wf_best_tuned) %>%
+  filter(.metric == 'rmse') %>%     
+  arrange(mean)
+
+glm_wf_final <- finalize_workflow(wf_best,
+                              select_best(wf_best_tuned, metric = 'rmse')) # In case everything needs to be rerun: rec3_rand_forest1
+glm_wf_final_fit <- last_fit(wf_final, index)                                  
+
+final_glm <- fit(glm_wf_final, data = airq_hospit)
+
+predictions_glm <- predict(final_glm, new_data = airq_lockdown)
+results_glm <- bind_cols(airq_lockdown, predictions_glm)
+
